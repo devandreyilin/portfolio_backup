fcbba38d0d20d4c847d41e2d18bf11a1b621eae8
ailindev <ailin.webdeveloper@gmail.com>
2019-05-27 20:24:58 +0500

    change callback form code

--- app/mail.php
+++ app/mail.php
@@ -12,11 +12,11 @@ $message = trim($_POST["message"]);
 $pattern = "/(content-type|bcc:|cc:|to:)/i";
 
 $to = 'ailin.webdeveloper@gmail.com';
-$sub = "=?utf-8?B?". base64_encode("topic of the letter"). "?="; // You can define email subject
+$sub = "=?utf-8?B?". base64_encode("Сообщение с сайта ailindev.ru"). "?="; // You can define email subject
 // HTML Elements for Email Body
-$body = "Name: $name<br>Email: $mail<br>Services: $services<br>Message: $message";
+$body = "Имя: $name<br>Почта: $mail<br>Услуга: $services<br>Сообщение: $message";
 //Must end on first column
-$headers = "From: ailin.webdeveloper@gmail.com" ."\r\n";
+$headers = "From: admin@ailindev.ru" ."\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n";
 // PHP email sender
 mail($to, $sub , $body, $headers);
